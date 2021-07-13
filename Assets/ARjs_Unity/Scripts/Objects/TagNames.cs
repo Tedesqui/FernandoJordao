9c7b177e45627cb89886ea70b041b19827e9a4ba
Tedesqui <87279924+Tedesqui@users.noreply.github.com>
2021-07-13 17:13:42 -0300

    Delete Assets directory

--- Assets/ARjs_Unity/Scripts/Objects/TagNames.cs
+++
@@ -1,8 +0,0 @@
-public static class TagNames {
-    public static string plane = "a-plane";
-    public static string video = "a-video";
-    public static string cube = "a-box";
-    public static string model = "a-entity";
-    public static string sphere = "a-sphere";
-    public static string cylinder = "a-cylinder";
-}
